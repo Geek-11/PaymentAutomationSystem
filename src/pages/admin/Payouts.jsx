80e122b2d16c40a9141300c5cd6d7f2c5c048d82
gopesh-goyal26 <gopeshgoyal26@gmail.com>
2025-05-17 21:01:30 +0530

    Update Payout Logic

--- src/pages/admin/Payouts.jsx
+++ src/pages/admin/Payouts.jsx
@@ -14,6 +14,7 @@ import { usePayout } from '@/hooks/usePayout';
 const Payouts = () => {
   const {payouts, setPayouts,isLoading} = usePayout();
   const { sessions } = useSessions();
+  console.log(payouts);
   console.log(sessions);
   
   const [isGenerateModalOpen, setIsGenerateModalOpen] = useState(false);
